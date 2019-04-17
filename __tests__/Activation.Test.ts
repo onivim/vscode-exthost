14b22f7d6249c790af9dfdbb3e62c315c29e9bd9
Bryan Phelps <bryphe@outlook.com>
2019-04-17 13:48:58 -0700

    Test Case: Activation Events (#12)
    * Add initial activation-event test extension
    
    * Add minimal activation
    
    * Add test case for activation events

--- __tests__/Activation.Test.ts
+++ __tests__/Activation.Test.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import * as ExtensionHost from "./ExtensionHost";
 
 let extensionPath = path.join(__dirname, "..", "test_extensions", "oni-api-tests", "package.json");
+let activationExtensionPath = path.join(__dirname, "..", "test_extensions", "oni-activation-events-tests", "package.json");
 
 describe("activation", () => {
 	test("get activation event for '*' activation type", async () => {
@@ -22,6 +23,50 @@ describe("activation", () => {
 
 			await api.start();
 
+			await promise;
+		});
+	});
+
+	test("activates in response to onLanguage event", async () => {
+		await ExtensionHost.withExtensionHost([activationExtensionPath], async (api) => {
+		let promise = new Promise((c) => {
+
+				api.onMessage.subscribe(msg => {
+					const { payload } = msg;
+
+					if(payload.methodName == "$onDidActivateExtension") {
+						console.dir(payload);
+						c();
+					}
+				});
+
+			});
+
+			await api.start();
+
+            api.sendNotification(["ExtHostExtensionService", "$activateByEvent", ["onLanguage:testlang"]]);
+			await promise;
+		});
+	});
+
+	test("activates in response to onLanguage event", async () => {
+		await ExtensionHost.withExtensionHost([activationExtensionPath], async (api) => {
+		let promise = new Promise((c) => {
+
+				api.onMessage.subscribe(msg => {
+					const { payload } = msg;
+
+					if(payload.methodName == "$onDidActivateExtension") {
+						console.dir(payload);
+						c();
+					}
+				});
+
+			});
+
+			await api.start();
+
+            api.sendNotification(["ExtHostExtensionService", "$activateByEvent", ["onCommand:extension.helloWorld"]]);
 			await promise;
 		});
 	});
