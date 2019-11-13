24131b816ad170844eb9eeaf3c3556fe4f6f75b9
Bryan Phelps <bryphe@outlook.com>
2019-11-13 15:15:23 -0800

    Script to pull in latest commits from VSCode

--- oni-cherry-pick.js
+++ oni-cherry-pick.js
@@ -13,31 +13,39 @@ const shell = (sz) => {
 
 const currentBranch = shell("git rev-parse --abbrev-ref HEAD");
 
-if (currentBranch !== "master") {
+/*if (currentBranch !== "master") {
 	console.error("Script must be run on MASTER");
 	process.exit(0);
-}
+}*/
 
 const log = (msg) => console.log("** INFO: " + msg);
 
 let lastCommit = fs.readFileSync("oni-last-cherry-pick").toString("utf8").trim();
 log("Last commit that was cherry-picked: " + lastCommit);
 
-shell("git checkout -b cherry-pick-" + lastCommit);
+//shell("git checkout -b cherry-pick-" + lastCommit);
 
-const BATCH_SIZE = 5;
+const BATCH_SIZE = 10;
 
 try {
 	while (true) {
 		for (let i = 0; i < BATCH_SIZE; i++) {
 			const commitToPick = shell(`cd ../vscode && git rev-list --ancestry-path ${lastCommit}..HEAD | tail -1`);
-			shell(`git cherry-pick ${commitToPick}`);
+			shell(`git merge ${commitToPick}`);
 			lastCommit = commitToPick;
 		}
 		
+		shell(`git log --oneline -n${BATCH_SIZE}`);
+		console.log("Running yarn install...");
 		shell(`yarn install`);
+		console.log("Running yarn compile...");
 		shell(`yarn compile`);
-		console.log("SUCCESS!");
+		console.log("Running tests...");
+		shell(`yarn test:oni`);
+		console.log("** SUCCESS! **");
+
+		console.log("writing commit to file: " + lastCommit);
+		fs.writeFileSync("oni-last-cherry-pick", lastCommit);
 	}
 } catch (ex) {
 	// We finally failed....
