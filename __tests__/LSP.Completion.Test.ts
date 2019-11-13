dfd46c48c4b0747ab72549507c25e56815d268d5
Bryan Phelps <bryphe@outlook.com>
2019-11-13 08:37:48 -0800

    Formatting

--- __tests__/LSP.Completion.Test.ts
+++ __tests__/LSP.Completion.Test.ts
@@ -8,33 +8,31 @@ describe.only("LSP", () => {
     describe("Completion", () => {
 
         it("registers suggest support", async () => {
-        await ExtensionHost.withExtensionHost([extensionPath], async (api) => {
-            let extensionActivationPromise = api.waitForMessageOnce("MainThreadExtensionService", "$onDidActivateExtension");
+            await ExtensionHost.withExtensionHost([extensionPath], async (api) => {
+                let extensionActivationPromise = api.waitForMessageOnce("MainThreadExtensionService", "$onDidActivateExtension");
 
-            let onRegisterSuggest = () => api.waitForMessageOnce("MainThreadLanguageFeatures", "$registerSuggestSupport", (v) => {
-                console.log("!!!! " + JSON.stringify(v));
-                return true;
+                let onRegisterSuggest = () => api.waitForMessageOnce("MainThreadLanguageFeatures", "$registerSuggestSupport", (v) => {
+                    console.log("onRegisterSuggest: " + JSON.stringify(v));
+                    return true;
+                });
+
+                await api.start();
+                await extensionActivationPromise;
+
+                let uri = {
+                    scheme: "file",
+                    path: "D:/test1.txt"
+                };
+
+                api.createDocument(uri, ["hello", "world"], "plaintext");
+
+
+                // Updating so the "GREETINGS" is all uppercase should trigger a diagnostics
+                api.updateDocument(uri, { startLineNumber: 1, endLineNumber: 1, startColumn: 1, endColumn: 6 }, "GREETINGS", 100);
+
+                await onRegisterSuggest();
+
             });
-
-            await api.start();
-            await extensionActivationPromise;
-
-            let uri = {
-                scheme: "file",
-                path: "D:/test1.txt"
-            };
-
-            api.createDocument(uri, ["hello", "world"], "plaintext");
-
-
-
-            // Updating so the "GREETINGS" is all uppercase should trigger a diagnostics
-            api.updateDocument(uri, {startLineNumber: 1, endLineNumber: 1, startColumn: 1, endColumn: 6}, "GREETINGS", 100);
-
-            await onRegisterSuggest();
-
-        });
-
         });
     });
 });
