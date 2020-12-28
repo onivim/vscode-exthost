3df9a02c32fade917dfb5abf9819eca097c14719
Bryan Phelps <bryphe@outrunlabs.com>
2020-12-28 14:17:24 -0800

    Update package-extensions script

--- build/oni/package-extensions.js
+++ build/oni/package-extensions.js
@@ -51,7 +51,6 @@ const extensionsToInclude = [
 	"markdown-basics",
 	"markdown-language-features",
 	"merge-conflict",
-	"npm",
 	"objective-c",
 	"perl",
 	"php",
@@ -86,12 +85,99 @@ const extensionsToInclude = [
 	"yaml",
 ];
 
+const pathsToRemove = [
+	"**/*.css",
+	"**/*.js.map",
+	"**/node_modules",
+	"css/test",
+	"cpp/test",
+	"typescript-basics/test",
+];
+
+const extensionWithDependencies = [
+	"css-language-features",
+	"css-language-features/server",
+	// "emmet",
+	"git",
+	// "grunt",
+	// "gulp,"
+	"html-language-features",
+	"html-language-features/server",
+	"json-language-features",
+	"json-language-features/server",
+	"markdown-language-features",
+	"merge-conflict",
+	"php-language-features",
+	"typescript-language-features",
+];
+
+// Copy all extensions...
+
 extensionsToInclude.forEach(extension => {
 	const extensionPath = path.join(extensionsDir, extension);
 	const outPath = path.join(publishDir, extension);
 	console.log(`Copying ${extensionPath} to ${outPath}...`)
 	fs.copySync(extensionPath, outPath);
 })
+
+// Merge all package.json into a single one
+
+const semver = require("semver");
+const sourcePackageJson = fs.readFileSync(
+	path.join(rootDir, "oni-extensions.package.json")
+);
+
+let extensionPackageJSON = JSON.parse(sourcePackageJson);
+let allDependencies = extensionPackageJSON.dependencies;
+extensionWithDependencies.forEach(extensionWithDependencies => {
+	const packageJsonPath = path.join(extensionsDir, extensionWithDependencies, "package.json");
+	console.log(`Reading dependencies for ${packageJsonPath}`);
+	const packageJsonFile = fs.readFileSync(packageJsonPath);
+	const newPackageJson = JSON.parse(packageJsonFile);
+	const newDependencies = newPackageJson.dependencies;
+
+	Object.keys(newDependencies).forEach((key) => {
+		// Do we already have this dependency?
+		if (allDependencies[key]) {
+			if (allDependencies[key] != newDependencies[key]) {
+				// Different version!
+				const originalVersion = allDependencies[key];
+				const newVersion = newDependencies[key];
+				let versionToUse;
+				if (semver.compare(semver.coerce(originalVersion), 
+					semver.coerce(newVersion)) > 0) {
+					// Using version
+					versionToUse = originalVersion;
+				} else {
+					console.log("Case 2");
+					versionToUse = newVersion;
+				}
+				console.log(`-- Using version ${versionToUse} for ${key}`);
+				allDependencies[key] = versionToUse;
+			} else {
+				// No-op - same dependency
+			}
+		} else {
+			allDependencies[key] = newDependencies[key]
+		}
+	})
+
+	// Delete the node_modules from this folder
+
+	const nodeModulesPath = path.join(extensionsDir, extensionWithDependencies, "node_modules");
+	console.log("Deleting path: " + nodeModulesPath);
+	rimraf.sync(nodeModulesPath);
+});
+
+
+const dependencies = JSON.stringify(allDependencies);
+console.log(`Final dependency set: ${dependencies}`)
+
+extensionPackageJSON.dependencies = allDependencies;
+fs.writeFileSync(
+	path.join(publishDir, "package.json"),
+	JSON.stringify(extensionPackageJSON, null, 2)
+);
 
 // console.log(`Copying ${outDir} to ${packageOutDir}...`)
 // fs.copySync(outDir, packageOutDir);
@@ -102,9 +188,8 @@ extensionsToInclude.forEach(extension => {
 // console.log(`Copying ${srcProductJson} to ${destProductJson}`);
 // fs.copySync(srcProductJson, destProductJson);
 
-
-// pathsToRemove.forEach((p) => {
-// 	const pathToRemove = path.join(packageOutDir, p);
-// 	console.log("Remove path: " + pathToRemove);
-// 	rimraf.sync(pathToRemove);
-// });
+pathsToRemove.forEach((p) => {
+	const pathToRemove = path.join(publishDir, p);
+	console.log("Remove path: " + pathToRemove);
+	rimraf.sync(pathToRemove);
+});
